7e71da4924882275ab2efc9e15f5e35e882e3104
edom18 <edo.m18@gmail.com>
2019-12-08 11:16:46 +0900

    feat: Added demo codes.

---
+++ Assets/TransformParticleSystem/Demo/Scripts/Hider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hider : MonoBehaviour
+{
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1f);
+
+        gameObject.SetActive(false);
+    }
+}
